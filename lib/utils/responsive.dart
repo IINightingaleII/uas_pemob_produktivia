3a003b8dff7ebc7af361d9f098e0b80c064de640
IINightingaleII <apasabun3@gmail.com>
2025-12-05 13:22:29 +0700

    implementasi firestore dan firebase storage

---
+++ lib/utils/responsive.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/widgets.dart';
+
+/// Utility class untuk responsive design
+/// Membantu aplikasi beradaptasi dengan berbagai ukuran layar
+class Responsive {
+  static const double mobileBreakpoint = 600;
+  static const double tabletBreakpoint = 900;
+  static const double desktopBreakpoint = 1200;
+
+  /// Get screen width
+  static double screenWidth(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  /// Get screen height
+  static double screenHeight(BuildContext context) {
+    return MediaQuery.of(context).size.height;
+  }
+
+  /// Check if device is mobile
+  static bool isMobile(BuildContext context) {
+    return screenWidth(context) < mobileBreakpoint;
+  }
+
+  /// Check if device is tablet
+  static bool isTablet(BuildContext context) {
+    final width = screenWidth(context);
+    return width >= mobileBreakpoint && width < tabletBreakpoint;
+  }
+
+  /// Check if device is desktop
+  static bool isDesktop(BuildContext context) {
+    return screenWidth(context) >= tabletBreakpoint;
+  }
+
+  /// Get responsive padding horizontal
+  static double paddingHorizontal(BuildContext context) {
+    if (isMobile(context)) {
+      return 16.0;
+    } else if (isTablet(context)) {
+      return 24.0;
+    } else {
+      return 32.0;
+    }
+  }
+
+  /// Get responsive font size
+  static double fontSize(BuildContext context, double baseSize) {
+    if (isMobile(context)) {
+      return baseSize;
+    } else if (isTablet(context)) {
+      return baseSize * 1.1;
+    } else {
+      return baseSize * 1.2;
+    }
+  }
+
+  /// Get responsive icon size
+  static double iconSize(BuildContext context, double baseSize) {
+    if (isMobile(context)) {
+      return baseSize;
+    } else if (isTablet(context)) {
+      return baseSize * 1.2;
+    } else {
+      return baseSize * 1.4;
+    }
+  }
+
+  /// Get responsive spacing
+  static double spacing(BuildContext context, double baseSpacing) {
+    if (isMobile(context)) {
+      return baseSpacing;
+    } else if (isTablet(context)) {
+      return baseSpacing * 1.2;
+    } else {
+      return baseSpacing * 1.5;
+    }
+  }
+
+  /// Get responsive width percentage
+  static double widthPercent(BuildContext context, double percent) {
+    return screenWidth(context) * (percent / 100);
+  }
+
+  /// Get responsive height percentage
+  static double heightPercent(BuildContext context, double percent) {
+    return screenHeight(context) * (percent / 100);
+  }
+
+  /// Get responsive value based on screen size
+  static T value<T>(
+    BuildContext context, {
+    required T mobile,
+    T? tablet,
+    T? desktop,
+  }) {
+    if (isDesktop(context) && desktop != null) {
+      return desktop;
+    } else if (isTablet(context) && tablet != null) {
+      return tablet;
+    } else {
+      return mobile;
+    }
+  }
+}
+
